d794a5a786b2731b02c36c73a44a09186fb184c1
Musa17 <amusul00@fesb.hr>
2021-08-26 16:52:05 +0200

    Renaming project

--- MultifunctionalApp/MultifunctionalApp.cs
+++ MultifunctionalApp/MultifunctionalApp.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MultifunctionalApp
 {
-    public partial class Form1 : Form
+    public partial class MultifunctionalApp : Form
     {
-        public Form1()
+        public MultifunctionalApp()
         {
             InitializeComponent();
         }
