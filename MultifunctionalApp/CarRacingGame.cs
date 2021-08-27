929aae4bdd19b794d3c7790c83232f953d0777ff
Musa17 <amusul00@fesb.hr>
2021-08-27 13:22:43 +0200

    Add moving lines on the road

--- MultifunctionalApp/CarRacingGame.cs
+++ MultifunctionalApp/CarRacingGame.cs
@@ -16,5 +16,56 @@ namespace MultifunctionalApp
         {
             InitializeComponent();
         }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            moveLine(5);
+        }
+
+        void moveLine(int speed)
+        {
+            if (pictureBox1.Top > 500)
+            {
+                pictureBox1.Top = 0;
+            }
+
+            else
+            {
+                pictureBox1.Top += speed;
+            }
+
+
+            if (pictureBox2.Top > 500)
+            {
+                pictureBox2.Top = 0;
+            }
+
+            else
+            {
+                pictureBox2.Top += speed;
+            }
+
+
+            if (pictureBox3.Top > 500)
+            {
+                pictureBox3.Top = 0;
+            }
+
+            else
+            {
+                pictureBox3.Top += speed;
+            }
+
+
+            if (pictureBox4.Top > 500)
+            {
+                pictureBox4.Top = 0;
+            }
+
+            else
+            {
+                pictureBox4.Top += speed;
+            }
+        }
     }
 }
